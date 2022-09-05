6a8eff3bc159a696be88e5949f2ba5b3597cc88b
Onur Dogan <iamonurdogan@gmail.com>
2022-09-05 16:26:01 +0300

    lists

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -9,6 +9,22 @@ export class ToDoComponent implements OnInit {
 
   constructor() { }
 
+  private name = "Onur";
+
+  // items = [
+  //   "item 1", "item 2", "item 3"
+  // ];
+
+  items = [
+    { id: 1, description: "breakfast", action: "yes" },
+    { id: 2, description: "workout", action: "yes" },
+    { id: 3, description: "shopping", action: "no" }
+  ];
+
+  getName() {
+    return this.name; 
+  }
+
   ngOnInit(): void {
   }
 
